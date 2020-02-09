dbfa901638b06296ddc3d797b172f87d9713fdf3
yimeishao <maria.yimei.shao@gmail.com>
2020-02-09 16:45:56 -0700

    get_user_date is functional

--- lib/bzn_poetry/cli.rb
+++ lib/bzn_poetry/cli.rb
@@ -4,7 +4,10 @@ class BznPoetry::CLI
     
     def welcome 
         puts "Welcome to Bozeman Craigslist Poetry"
+        @input = ""
+        until @input == "exit"
         main_menu 
+        end 
     end 
 
     def main_menu
@@ -22,7 +25,7 @@ class BznPoetry::CLI
     def poems_menu
         puts "Select an available date by selecting its index number"
         get_available
-        # get_user_date
+        get_user_date
     end 
 
     def get_available
@@ -36,35 +39,32 @@ class BznPoetry::CLI
       @available.each do |date| 
         if @list.include?(date) == false && @list.length < 10 
         @list << date 
-    end 
-    end 
+        end 
+        end 
+
       @list.each_with_index do |date, i|
       puts "#{i + 1}. #{date}"
       end 
     end 
     
-    # def get_user_date 
-    #    user_date = gets.strip
-    #    if @available.index.to_s.include?(user_date)
-    #    show_poem(user_date)
-    #     elsif user_date == "exit"
-    #         exit 
-    #     else 
-    #         invalid_input
-    #     end 
-    # end 
+    def get_user_date 
+       user_date = gets.strip.to_i
+       if user_date.to_i.between?(1, 10)
+        user_date = user_date.to_i - 1 
+        show_poem(user_date)
+       else 
+        invalid_input 
+       end 
+    end 
 
-    # def show_poem(chosen_date)
-    # if the chosen_date matches the date attribute 
-    # puts text 
-    # end 
-    # end
+    def show_poem(user_date)
+        date = @available[user_date]
+        puts "Here is the poem for #{date}:"
+    end 
 
     def invalid_input
     puts "Command not recognized"
     main_menu
     end 
 
-       # def exit 
-    # end 
 end
