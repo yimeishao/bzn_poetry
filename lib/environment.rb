575eed6db38354264a3af117a33d072f166a9b8b
yimeishao <maria.yimei.shao@gmail.com>
2020-02-11 18:15:27 -0700

    deleted superfluous files from environment

--- lib/environment.rb
+++ lib/environment.rb
@@ -7,8 +7,6 @@ require_relative "./bzn_poetry/version"
 require_relative './bzn_poetry/cli'
 require_relative './bzn_poetry/post'
 require_relative './bzn_poetry/scraper'
-# require_relative './bzn_poetry/posts'
-# require_relative './bzn_poetry/poems'
 
 module BznPoetry
   class Error < StandardError; end
